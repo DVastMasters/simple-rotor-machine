9f45032aaef31b5f98919256f2abc14fb15c674b
DVastMasters <contatorafaeldutra@outlook.com>
2023-10-22 17:24:16 -0300

    feat: robin

---
+++ rotor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define CIPHER 'C'
+#define DECIPHER 'D'
+
+
+int main(int argc, char *argv[]){
+
+   
+    return 0;
+}
