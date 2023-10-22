44718008fda8a7bea2ebf0ea7e0e69b59f981779
DVastMasters <contatorafaeldutra@outlook.com>
2023-10-22 17:24:42 -0300

    feat: swap, initializeRotor and printRotor

--- rotor.c
+++ rotor.c
@@ -5,6 +5,35 @@
 
 #define CIPHER 'C'
 #define DECIPHER 'D'
+ 
+
+void initializeRotor(unsigned char* s)
+{
+    for(int i=0; i < 256; i++)
+    {
+        s[i] = i;
+    }    
+}
+
+void swap(unsigned char* firstElement, unsigned char* secondElement)
+{
+    unsigned char tmp = *firstElement;
+    *firstElement = *secondElement;
+    *secondElement = tmp;
+}
+
+void printRotor(unsigned char* arr)
+{
+    for (int i=0; i<256; i++)
+    {
+        if(i%16==0 && i!=0)
+        {
+            printf("\n");
+        }
+        printf("%03d ", arr[i]);        
+    }
+}
+
 
 
 int main(int argc, char *argv[]){
